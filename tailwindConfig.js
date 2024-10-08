9710679d50cbfee09284ba10c57598bdc5ea39d1
nhurudheen <ifaniyioluwapelumi@gmail.com>
2024-10-08 18:20:46 +0100

    Dashboard Adjusted and Tailwind Config

---
+++ tailwindConfig.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+    theme: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      extend: {
+        colors: {
+          clifford: "#da373d",
+          lightblue: "rgb(173, 216, 230)",
+          skyblue: "rgb(135, 206, 235)",
+          powderblue: "rgb(176, 224, 230)",
+          steelblue: "rgb(70, 130, 180)",
+          cornflowerblue: "rgb(100, 149, 237)",
+          white: "rgb(255, 255, 255)",
+        },
+      },
+    },
+  };
